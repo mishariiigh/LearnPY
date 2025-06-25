aaf0a0d85b418291f3ec48524823d6b1ff2a41be
mishariiigh <mishariiigh@hotmail.com>
2025-06-25 16:19:18 -0400

    update

---
+++ Practice/airthmetic.py
@@ -0,0 +1,40 @@
+#friends = 10
+#friends = friends + 1
+# friends =+ 1
+# friends = friends - 2
+# friends -= 2
+# friends *= 5
+#friends = friends / 2
+#friends /=2
+# friends = friends ** 2 #to the power of 2 = 5 * 5 because friends = 5
+# friends **=2 # same thing as above it
+
+#remainder = friends % 3 # 10 remainder of 3 is 1 because 3*3=9 not 10
+
+#print(friends)
+
+#######################
+#x = 3.14
+#y = 4
+#z = 5
+#result = round(x)
+#result = abs(y) #abs means absolute value of y
+#result = pow(4, 3) #what y to the power of 3
+#result = max(x, y, z) #maximum value between x, y and z
+#result = min(x, y, z) #minimum value between x, y and z
+
+#print(result)
+######################
+
+#import math
+#print(math.pi) # = 3.141492654
+#print(math.e) #people work with physics = 2.718...
+#x = 9.1
+#result = math.sqrt(x) # square root of x
+#result = math.ceil(x) #rounded up which makes 9.1 a 10
+#result = math.floor(x) #rounded down which makes 9.1 a 9
+
+
+#print(result)
+###################################
+
