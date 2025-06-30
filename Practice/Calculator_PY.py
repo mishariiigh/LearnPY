bd517f592cd1df55fad895deb5ea64e6df2e3069
mishariiigh <mishariiigh@hotmail.com>
2025-06-30 13:22:03 -0400

    update

---
+++ Practice/Calculator_PY.py
@@ -0,0 +1,19 @@
+#Python Caculator
+operator = input("Enter an operator (+ - * /):")
+num1 = float(input("enter first number:"))
+num2 = float(input("enter second number"))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not a valid operator")
